55f03751c91062b2c00eb81cf86e1ecb6c8c5e79
Hieueee <hieubui457@gmail.com>
2022-10-18 02:09:37 -0700

    Project 4 problem 2
    - finished fixed point approximation
    - need to solve the divergence issue with residual
    - newton method is broken

--- Project 4/ENG180_PJ4_main_Hieu_Bui.m
+++ Project 4/ENG180_PJ4_main_Hieu_Bui.m
@@ -12,24 +12,31 @@ clear;
 
 k = 8;
 figure(2)
-subplot(3,1,1)
-r2a1=p2f('a',k,.5,3,.7);
-title('Propellant Burnt when n = .5')
+% subplot(4,1,1)
+% [r11,r21]=p2f('a',k,.5,3,.7);
+% title('Propellant Burnt when n = .5')
+% xlabel('x')
+% ylabel('y')
+% hold on;
+% subplot(4,1,2)
+% [r12,r22]=p2f('a',k,2.1,1,.6);
+% title('Propellant Burnt when n = 2.1')
+% xlabel('x')
+% ylabel('y')
+% hold on;
+% subplot(4,1,3)
+% [r13,r23]=p2f('b',k,1,.7,.6);
+% title('Semenov Equation')
+% xlabel('x')
+% ylabel('y')
+% hold on;
+% subplot(4,1,4)
+[newtonR11, newtonR22]=p2n('a',k,.5,3,2);
+title('Propellant Burnt when n=.5 using Newton')
 xlabel('x')
 ylabel('y')
 hold on;
-subplot(3,1,2)
-r2a2=p2f('a',k,2.1,1,.6);
-title('Propellant Burnt when n = 2.1')
-xlabel('x')
-ylabel('y')
-hold on;
-subplot(3,1,3)
-r2b=p2f('b',k,1,.7,.6);
-title('Semenov Equation')
-xlabel('x')
-ylabel('y')
-hold on;
+
 
 % Main function for problem 2A
 % e = which equation ['a' = 2a; 'b' = 2b]
@@ -47,8 +54,7 @@ function [r1,r2] = p2f(e,k,n,s,g)
     alpha = 2;
     r = zeros(k,1);
     x = ones(k,1);
-    r(1) = g;
-    x(1) = r(1);
+    x(1) = g;
     if strcmp(e,'a') 
         %===========root 1===========%
         for i = 1:k-1
@@ -95,7 +101,7 @@ function [r1,r2] = p2f(e,k,n,s,g)
         r2 = r(k);        
     elseif strcmp(e,'b')
         for i = 1:k
-            r(i) = (c*exp(1)^(-alpha*x(i))-a)/b; %semenov root equation
+            r(i) = (c*exp(1)^(-alpha*x(i))-a)/b; %semenov root equation 1
             x(i+1) = r(i);
         end
         r(k) = (c*exp(1)^(-alpha*x(k))-a)/b;
@@ -115,51 +121,105 @@ function [r1,r2] = p2f(e,k,n,s,g)
             hold on;
         end
         r1 = r(k);
+
+        for i = 1:k
+            r(i) = -alpha/(log((alpha+b*x(i))/c)); %semenov root equation 2
+            x(i+1) = r(i);
+        end
+        r(k) = -alpha/(log((alpha+b*x(k))/c));
+        y = linspace(.4,s,100);
+        rexact2 = -alpha./(log((alpha+b.*y)./c));
+
+        plot(y,y,y,rexact2)
+        xline(g);
+        hold on;
+        plot(x(k),r(k),'^','MarkerFaceColor','red')
+        hold on;
+        r2 = r(k);
     end
 end
 
 function [r1,r2] = p2n(e,k,n,s,g)
     c1 = 1.6;
     c2 = 1.1;
-    %     a = -.13;
-    %     b = .44;
-    %     c = .25;
-    %     alpha = 2;
-
-    x = ones(k,1);
-    r = ones(k,1);
-
-    for i=1:k-1
-        r(i) = (c1/c2)*x(i)^n; %root function 1
-        rdot = (n*c1/c2)*x(i)^(n-1);
-        x(i+1) = x(i) - r(i)/rdot;
+    a = -.13;
+    b = .44;
+    c = .25;
+    alpha = 2;
+    if strcmp(e,'a')
+        x = ones(k,1);
+        r = ones(k,1);
+        x(1) = g;
+        for i=1:k-1
+            r(i) = (c1/c2)*x(i)^n; %root function 1
+            rdot = (n*c1/c2)*x(i)^(n-1);
+            x(i+1) = x(i) - r(i)/rdot;
+        end
+        r(k) = (c1/c2)*x(k)^n;
+        
+        y = linspace(-2,s,100);
+        rexact = (c1/c2).*y.^n;
+        
+        plot(y,y,y,rexact)
+        xline(x(1))
+        hold on; grid on;
+        plot(x(k),r(k),'bo')
+        hold on;
+        r1 = r(k);
+    
+        for i=1:k-1
+            r(i) = (c2/c1*x(i))^(1/n);
+            rdot = (c2/c1/n)*(c2/c1*x(i))^((1-n)/n);
+            x(i+1) = x(i) - r(i)/rdot;
+        end
+        r(k) = (c2/c1*x(k))^(1/n);
+        
+        y = linspace(-2,s,100);
+        rexact = (c2/c1).*y.^(1/n);
+        
+        plot(y,y,y,rexact)
+        xline(x(1))
+        hold on; grid on;
+        plot(x(k),r(k),'bo')
+        hold on;
+        r2 = r(k);
+    elseif strcmp(e,'b')
+        x = ones(k,1);
+        r = ones(k,1);
+        x(1) = g;
+        %====root equation 1 for semenov====%
+        for i = 1:k-1
+            r(i) = (c*exp(1)^(-alpha/x(i))-a)/b;
+            rdot = alpha*c/b*x(i)^2*e^(-alpha/x(i));
+            x(i+1) = x(i) - r(i)/rdot;
+        end
+        r(k) = (c*exp(1)^(-alpha/x(k))-a)/b;
+        
+        y = linspace(-2,s,100);
+        rexact = (c.*exp(1).^(-alpha./y)-a)./b;
+        
+        plot(y,y,y,rexact)
+        xline(g)
+        hold on; grid on;
+        plot(x(k),r(k),'bo')
+        hold on;
+        r1 = r(k);
+        
+%         %====root 2 for semenov====%
+%         for i = 1:k-1
+%             r(i) = -alpha*(log((alpha+b*x(i))/c))^-1;
+%             rdot = alpha*b/c*(log((alpha+b*x(i))/c))^-2;
+%             x(i+1) = x(i) - r(i)/rdot;
+%         end
+%         r(k) = -alpha*log((alpha+b*x(k))/c)^-1;
+%         y = linspace(-2,s,100);
+%         rexact2 = -alpha.*log((alpha+b.*y)./c).^-1;
+%         
+%         plot(y,y,y,rexact2)
+%         xline(g)
+%         hold on; grid on;
+%         plot(x(k),r(k),'bo')
+%         hold on;
+        r2 = r(k);
     end
-    r(k) = (c1/c2)*x(k)^n;
-    
-    y = linspace(-2,2,100);
-    rexact = (1.6/1.1).*y.^n;
-    
-    plot(y,y,y,rexact)
-    xline(x(1))
-    hold on; grid on;
-    plot(x(k),r(k),'bo')
-    hold on;
-    r1 = r(k);
-
-    for i=1:k-1
-        r(i) = (c2/c1*x(i))^(1/n);
-        rdot = (c2/c1/n)*(c2/c1*x(i))^((1-n)/n);
-        x(i+1) = x(i) - r(i)/rdot;
-    end
-    r(k) = (c2/c1*x(k))^(1/n);
-    
-    y = linspace(-2,2,100);
-    rexact = (c2/c1).*y.^(1/n);
-    
-    plot(y,y,y,rexact)
-    xline(x(1))
-    hold on; grid on;
-    plot(x(k),r(k),'bo')
-    hold on;
-    r2 = r(k);
 end
--- Project 4/testing.m
+++ Project 4/testing.m
@@ -5,26 +5,28 @@ k = 8;
 
 c1 = 1.6;
 c2 = 1.1;
-%     a = -.13;
-%     b = .44;
-%     c = .25;
-%     alpha = 2;
+a = -.13;
+b = .44;
+c = .25;
+alpha = 2;
 x = ones(k,1);
 r = ones(k,1);
-x(1) = 1.6;
+x(1) = .2;
+n = 2;
 
-for i=1:k-1
-    r(i) = (c2/c1*x(i))^(1/2.1);
-    rdot = (c2/c1/2.1)*(c2/c1*x(i))^((1-2.1)/2.1);
+for i = 1:k-1
+    r(i) = (c*exp(1)^(-alpha/x(i))-a)/b;
+    rdot = (alpha*c/(b*x(i)^2))*exp(1)^(-alpha/x(i));
     x(i+1) = x(i) - r(i)/rdot;
 end
-r(k) = (c2/c1*x(k))^(1/2.1);
+r(k) = (c*exp(1)^(-alpha/x(k))-a)/b;
 
 y = linspace(-2,2,100);
-rexact = (1.6/1.1).*y.^2;
+rexact = (c.*exp(1).^(-alpha./y)-a)./b;
+
 figure(1);
 plot(y,y,y,rexact)
 xline(x(1))
 hold on; grid on;
-plot(x(k),r(k-1),'bo')
-hold on;
+% plot(x(k),r(k-1),'bo')
+% hold on;
