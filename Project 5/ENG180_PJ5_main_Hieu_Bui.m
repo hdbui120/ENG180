b8858b009ca6bf8449316cc98c83be941884dbdd
Hieueee <hieubui457@gmail.com>
2022-10-26 18:03:21 -0700

    update pj5

--- Project 5/ENG180_PJ5_main_Hieu_Bui.m
+++ Project 5/ENG180_PJ5_main_Hieu_Bui.m
@@ -17,7 +17,14 @@ res2C1 = finiteDiff2(delta,pi/3,'central');
 res2F2 = finiteDiff2(delta,pi/3,'forward2');
 res2B2 = finiteDiff2(delta,pi/3,'backward2');
 
-fig1 = figure('Name','Residual Plot 1st Order')
+% Compute 3rd order derivative residuals
+res3F1 = finiteDiff3(delta,pi/3,'forward1');
+res3B1 = finiteDiff3(delta,pi/3,'backward1');
+res3C1 = finiteDiff3(delta,pi/3,'central');
+res3F2 = finiteDiff3(delta,pi/3,'forward2');
+res3B2 = finiteDiff3(delta,pi/3,'backward2');
+
+figure('Name','Residual Plot 1st Order')
 loglog(res1F1,'Marker','o')
 hold on
 loglog(res1B1,'Marker','*')
@@ -32,7 +39,7 @@ xlabel('\pi/dx')
 ylabel('Residuals')
 legend('Forward','Backward','Central','2nd Order Forward','2nd Order Backward')
 
-fig2 = figure('Name','Residual Plot 2nd Order')
+figure('Name','Residual Plot 2nd Order')
 loglog(res2F1,'Marker','o')
 hold on
 loglog(res2B1,'Marker','*')
@@ -47,7 +54,20 @@ xlabel('\pi/dx')
 ylabel('Residuals')
 legend('Forward','Backward','Central','2nd Order Forward','2nd Order Backward')
 
-
+figure('Name','Residual Plot 3rd Order')
+loglog(res3F1,'Marker','o')
+hold on
+loglog(res3B1,'Marker','*')
+hold on
+loglog(res3C1,'Marker','diamond')
+hold on
+loglog(res3F2,'Marker','<')
+hold on
+loglog(res3B2,'Marker','+')
+title('Problem 1a: Third Derivative of f(x) = $sin^2(x)$ at $x=\frac{\pi}{3}$','Interpreter','latex')
+xlabel('\pi/dx')
+ylabel('Residuals')
+legend('Forward','Backward','Central','2nd Order Forward','2nd Order Backward')
 
 function residual = finiteDiff(deltax,x,style)    
     % given function
@@ -106,6 +126,36 @@ function residual = finiteDiff2(delta,x,style)
         case 'backward2'
             % Second order of accurate 
             diffF = (2.*f(x)-5.*f(x-delta)+4.*f(x-2.*delta)-f(x-3.*delta))./delta.^2;
+            residual = abs(dfdx-diffF);
+    end
+end
+
+function residual = finiteDiff3(delta,x,style)
+    % given function
+    f = @(x) sin(x).^2;
+    exactDiff = @(x) -8.*cos(x).*sin(x);
+    dfdx = exactDiff(x);
+    
+    switch style
+        case 'forward1'
+            % First order of accurate
+            diffF = (-f(x)+3.*f(x+delta)-3.*f(x+2.*delta)+f(x+3.*delta))./delta.^3;
+            residual = abs(dfdx-diffF);
+        case 'backward1'
+            % First order of accurate
+            diffF = (f(x)-3.*f(x-delta)+3.*f(x-2.*delta)-f(x-3.*delta))./delta.^3;
+            residual = abs(dfdx-diffF);
+        case 'central'
+            % Second order of accurate central
+            diffF = (-.5.*f(x-2.*delta)+f(x-delta)+f(x)-f(x+delta)+.5.*f(x+2.*delta))./delta.^2;
+            residual = abs(dfdx-diffF);
+        case 'forward2'
+            % Second order of accurate 
+            diffF = (-5/2.*f(x)+9.*f(x+delta)-12.*f(x+2.*delta)+7.*f(x+3.*delta)-3/2.*f(x+4.*delta))./delta.^2;
+            residual = abs(dfdx-diffF);
+        case 'backward2'
+            % Second order of accurate 
+            diffF = (5/2.*f(x)-9.*f(x-delta)+12.*f(x-2.*delta)-7.*f(x-3.*delta)+3/2.*f(x-4.*delta))./delta.^2;
             residual = abs(dfdx-diffF);
     end
 end
