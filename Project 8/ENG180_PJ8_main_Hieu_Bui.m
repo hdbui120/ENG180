09fc1b5ecfe4ae6712fe0aa8479f76289b9c1d87
Hieueee <hieubui457@gmail.com>
2022-11-16 01:49:37 -0800

    Project 8: mixed partial initial boundary value problems

---
+++ Project 8/ENG180_PJ8_main_Hieu_Bui.m
@@ -0,0 +1,84 @@
+clc, clear; 
+close all;
+
+%{
+    ENG 180 Project 8
+    File:           ENG180_PJ8_main_Hieu_Bui.m
+    Author:         Hieu Bui
+    Date:           11/14/2022
+    Description:    Solving mixed partial initial boundary value problems.
+
+%}
+
+[x,t,u] = heatEqn('fe');
+imagesc(t,x,u')
+set(gca,'Ydir','normal')
+title('Problem 1a: Heat Equation with Forward Euler')
+xlabel('$t$', 'Interpreter','latex')
+ylabel('$x$','Interpreter','latex')
+colorbar;
+
+%====Problem 1====%
+% 1A Heat Equation: du/dt = k*d2u/dt2; k = .4; 0<=t<=pi
+% generate u(x,0) with given function
+% solve for the next time step
+% solve for u(x,t_n+1) with tridiagonal system
+% solve for next time step
+function [x,t,u] = heatEqn(option)
+
+%====grid====%
+n = 101;
+x = linspace(0,pi,n);
+deltax = x(2);
+deltat = .9*(deltax^2/2);
+t = 0:deltat:pi;
+k = .4;
+
+switch option
+    case 'fe'
+        initialTemp = @(x) sin(x);
+        alp = (deltat*k)/(deltax^2);
+        solver = @(u0,u1,u2,alp) alp*(u0-2*u1+u2)+u1;
+        u = zeros(length(t),n);
+        u(1,:) = initialTemp(x); % temperature at time 0 for all x
+        u(:,n) = 0;
+        for j = 1:length(t)-1 % going along t   
+            for i = 2:n-1 % going along x
+                u0 = u(j,i-1);
+                u1 = u(j,i);
+                u2 = u(j,i+1);
+                u(j+1,i) = solver(u0,u1,u2,alp);
+            end
+         
+        end
+end
+end
+
+% supporting function
+function x = THOMAS3(a,b,c,d,n)
+
+    %initial condition
+    bbar(1) = b(1);
+    cbar(1) = c(1);
+    dbar(1) = d(1);
+
+    %making upper triangle
+    for i = 2:n
+        multiplier = a(i)./bbar(i-1);
+        abar(i) = a(i) - bbar(i-1).*multiplier;
+        bbar(i) = b(i) - cbar(i-1).*multiplier;
+        cbar(i) = c(i);
+        dbar(i) = d(i) - dbar(i-1).*multiplier;
+    end
+    
+    %initialize x of size n
+    x = ones(1,n);
+
+    %initialize end condition
+    x(n) = dbar(n)/bbar(n);
+
+    % Upward substitution AKA zip it up
+    for i = n-1:-1:1
+        x(i) = (dbar(i)-(cbar(i)*x(i+1)))/bbar(i);
+    end
+end
